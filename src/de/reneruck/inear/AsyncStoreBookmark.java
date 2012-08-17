e6b8cd936bab3447828614d353532a07f96d7e73
Rene <reneruck@googlemail.com>
2012-08-17 21:03:11 +0200

    fix update of bookmark

--- src/de/reneruck/inear/AsyncStoreBookmark.java
+++ src/de/reneruck/inear/AsyncStoreBookmark.java
@@ -2,6 +2,7 @@ package de.reneruck.inear;
 
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 
@@ -26,13 +27,23 @@ public class AsyncStoreBookmark extends AsyncTask<Bookmark, Void, Void> {
 			values.put(DbConfigs.FIELD_AUDIOBOOK_NAME, bookmarkToStore.getBookTitle());
 			values.put(DbConfigs.FIELD_TRACK, bookmarkToStore.getTrackNumber());
 			values.put(DbConfigs.FIELD_PLAYBACK_POS, bookmarkToStore.getPlaybackPosition());
-			long result = writableDatabase.insertWithOnConflict(DbConfigs.TABLE_BOOKMARKS, null, values , SQLiteDatabase.CONFLICT_ROLLBACK);
-			if(result == -1)
+			if(!bookmarkExists(writableDatabase, bookmarkToStore.getBookTitle()))
 			{
+				long result = writableDatabase.insertWithOnConflict(DbConfigs.TABLE_BOOKMARKS, null, values , SQLiteDatabase.CONFLICT_ROLLBACK);
+				
+			} else {
 				writableDatabase.updateWithOnConflict(DbConfigs.TABLE_BOOKMARKS, values, DbConfigs.FIELD_BOOKMARK_ID + "=" + bookmarkToStore.getId(), null,SQLiteDatabase.CONFLICT_ROLLBACK);
 			}
+			writableDatabase.close();
 		}
 		return null;
+	}
+
+	private boolean bookmarkExists(SQLiteDatabase db, String bookName) {
+		Cursor c = db.query(DbConfigs.TABLE_BOOKMARKS, new String[]{"*"}, DbConfigs.FIELD_AUDIOBOOK_NAME + " like '" + bookName + "'", null, null, null,null);
+		boolean result = c.getCount() > 0 ? true : false;
+		c.close();
+		return result;
 	}
 
 }
