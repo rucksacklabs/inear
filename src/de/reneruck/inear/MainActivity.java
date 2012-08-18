a3a941918067fa26772605b1e71e24bd16c4abe5
Rene <reneruck@googlemail.com>
2012-08-18 13:13:30 +0200

    organize imports

--- src/de/reneruck/inear/MainActivity.java
+++ src/de/reneruck/inear/MainActivity.java
@@ -5,14 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,7 +18,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import de.reneruck.inear.file.FileScanner;
 import de.reneruck.inear.settings.SettingsActivity;
 
 public class MainActivity extends Activity {
